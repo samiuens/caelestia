309d6589efef2f5b7c70508d345b640dab9b8972
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-09-17 14:21:14 +0200

    ⚰️ remove home manager module

--- flake.nix
+++ flake.nix
@@ -16,45 +16,53 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    forAllSystems = fn:
-      nixpkgs.lib.genAttrs nixpkgs.lib.platforms.linux (
-        system: fn nixpkgs.legacyPackages.${system}
-      );
-  in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      forAllSystems =
+        fn: nixpkgs.lib.genAttrs nixpkgs.lib.platforms.linux (system: fn nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-    packages = forAllSystems (pkgs: rec {
-      caelestia-shell = pkgs.callPackage ./nix {
-        rev = self.rev or self.dirtyRev;
-        stdenv = pkgs.clangStdenv;
-        quickshell = inputs.quickshell.packages.${pkgs.system}.default.override {
-          withX11 = false;
-          withI3 = false;
+      packages = forAllSystems (pkgs: rec {
+        caelestia-shell = pkgs.callPackage ./nix {
+          rev = self.rev or self.dirtyRev;
+          stdenv = pkgs.clangStdenv;
+          quickshell = inputs.quickshell.packages.${pkgs.system}.default.override {
+            withX11 = false;
+            withI3 = false;
+          };
+          app2unit = pkgs.callPackage ./nix/app2unit.nix { inherit pkgs; };
+          caelestia-cli = inputs.caelestia-cli.packages.${pkgs.system}.default;
         };
-        app2unit = pkgs.callPackage ./nix/app2unit.nix {inherit pkgs;};
-        caelestia-cli = inputs.caelestia-cli.packages.${pkgs.system}.default;
-      };
-      with-cli = caelestia-shell.override {withCli = true;};
-      debug = caelestia-shell.override {debug = true;};
-      default = caelestia-shell;
-    });
+        with-cli = caelestia-shell.override { withCli = true; };
+        debug = caelestia-shell.override { debug = true; };
+        default = caelestia-shell;
+      });
 
-    devShells = forAllSystems (pkgs: {
-      default = let
-        shell = self.packages.${pkgs.system}.caelestia-shell;
-      in
-        pkgs.mkShell.override {stdenv = shell.stdenv;} {
-          inputsFrom = [shell shell.plugin shell.extras];
-          packages = with pkgs; [material-symbols rubik nerd-fonts.caskaydia-cove];
-          CAELESTIA_XKB_RULES_PATH = "${pkgs.xkeyboard-config}/share/xkeyboard-config-2/rules/base.lst";
-        };
-    });
-
-    homeManagerModules.default = import ./nix/hm-module.nix self;
-  };
+      devShells = forAllSystems (pkgs: {
+        default =
+          let
+            shell = self.packages.${pkgs.system}.caelestia-shell;
+          in
+          pkgs.mkShell.override { stdenv = shell.stdenv; } {
+            inputsFrom = [
+              shell
+              shell.plugin
+              shell.extras
+            ];
+            packages = with pkgs; [
+              material-symbols
+              rubik
+              nerd-fonts.caskaydia-cove
+            ];
+            CAELESTIA_XKB_RULES_PATH = "${pkgs.xkeyboard-config}/share/xkeyboard-config-2/rules/base.lst";
+          };
+      });
+    };
 }
